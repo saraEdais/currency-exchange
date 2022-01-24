a6eb84a708799d3843b5893eab9e6c49d5f6f677
sara edais <saraedais99@gmail.com>
2022-01-24 17:49:21 +0200

    PSEUAG-109-i've added some of icons to send comp.

--- currency-exchange-ui/src/components/send/send.component.js
+++ currency-exchange-ui/src/components/send/send.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from "./send.module.css";
-import {CheckCircleOutlined, BankOutlined} from "@ant-design/icons"
+import { CheckCircleOutlined, BankOutlined } from "@ant-design/icons"
 import SendForm from '../sendForm/sendForm.component';
 
 const Send = () => {
 
   return <div className={classes.sendComponent}>
     <div className={classes.sendForm}>
-      <SendForm/>
+      <SendForm />
     </div>
     <div className={classes.description}>
       <h2>The fast & trusted way to send money</h2>
@@ -15,26 +15,38 @@ const Send = () => {
       <ul>
         <li>
           <div>
-          <CheckCircleOutlined className={classes.icon}/>
-          See our low fees and exchange rates in real-time
+            <CheckCircleOutlined className={classes.CheckCircleOutlinedIcon} />
+            See our low fees and exchange rates in real-time
           </div>
         </li>
         <li>
           <div>
-          <CheckCircleOutlined className={classes.icon}/>
-          Transparent delivery times so you can move money fast
+            <CheckCircleOutlined className={classes.CheckCircleOutlinedIcon} />
+            Transparent delivery times so you can move money fast
           </div>
         </li>
         <li>
           <div>
-          <CheckCircleOutlined className={classes.icon}/>
-          Sign up for a free personal or business account in minutes
+            <CheckCircleOutlined className={classes.CheckCircleOutlinedIcon} />
+            Sign up for a free personal or business account in minutes
           </div>
         </li>
       </ul>
       <div className={classes.bankIconDiv}>
-      <BankOutlined className={classes.bankIcon}/>
-    </div>
+      <div className={classes.icon} style={{backgroundColor:"rgb(26, 31, 113)",color:"white"}}>
+          VISA
+        </div>
+        <div className={classes.icon}>
+          EFT
+        </div>
+        <div className={classes.icon}>
+          ACH
+        </div>
+        <div className={classes.icon}>
+          WIRE
+        </div>
+        <BankOutlined className={classes.bankIcon} />
+      </div>
     </div>
   </div>;
 };
