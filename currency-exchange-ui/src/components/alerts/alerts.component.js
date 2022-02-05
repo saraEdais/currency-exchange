2e6c9fe762cc62ea2d7dda0c287273282cf1b79d
sara edais <saraedais99@gmail.com>
2022-02-05 20:45:48 +0200

    i've add alert component

---
+++ currency-exchange-ui/src/components/alerts/alerts.component.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import classes from "./alerts.module.css";
+import alertImage from '../../assets/images/alert.PNG'
+import Button from '../button/button.component';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Alerts = () => {
+
+    const [email, setEmail] = useState("");
+    const [isEntered, setIsEntered] = useState(false);
+
+    const notifyHandler = () => {
+        if (email.length === 0) {
+            setIsEntered(true)
+            console.log(isEntered)
+        }
+        else {
+            setIsEntered(false)
+            toast.success('successful subscribe', {
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored'
+            })
+        }
+        setEmail("")
+    }
+
+    return <div className={classes.alertsComponent}>
+        <div className={classes.subscribePart}>
+            <ToastContainer />
+            <div>
+                <label>Email</label>
+                <input
+                    className={isEntered? classes.alertInputEnter:classes.alertInput}
+                    type="text"
+                    placeholder='enter your email'
+                    value={email}
+                    onChange={(e) => { setEmail(e.target.value) }} />
+                {
+                    (isEntered && <div style={{color:"red",fontSize:"10px",marginBottom:"40px"}}>you should enter your email</div>)
+                }
+            </div>
+            <Button buttonName={"subscribe"} onClickHandler={notifyHandler} />
+        </div>
+        <div>
+            <img className={classes.alertImg} src={alertImage} />
+        </div>
+    </div>;
+};
+
+export default Alerts;
