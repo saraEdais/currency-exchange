75d2a34901a50c551a89c801b08fbbce5243268f
sara edais <saraedais99@gmail.com>
2022-02-05 20:54:42 +0200

    I've added home component

--- currency-exchange-ui/src/pages/home/home.component.js
+++ currency-exchange-ui/src/pages/home/home.component.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import Send from '../../components/send/send.component';
+import React, { useState } from 'react';
+import classes from './home.module.css'
+import Container from '../../components/container/container.component';
 
-const Home= () => {
-  return <div>
-      <Send/>
+const Home = () => {
+  const [selected, setSelected] = useState("convert");
+
+  const selectedHandler = (selectName) => {
+    console.log(selected)
+    setSelected(selectName)
+  }
+
+  return <div className={classes.homeComponent}>
+    <Container
+      selected={selected}
+      selectedHandler={selectedHandler} />
   </div>;
 };
 
